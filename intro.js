99b83404d8a0492548ef2473a0227ccc0d4b8d1a
thee freelancer <ronaldssebalamu2@gmail.com>
2023-07-17 02:44:25 +0300

    added the intro file

---
+++ intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "Ronald",
+    age: 10
+};
+var email = user.email;
+console.log(email);
---
+++ intro.ts
@@ -0,0 +1,10 @@
+let user = {
+        name: "Ronald",
+        age: 10
+}
+let email = user.email
+
+let myNum = 6
+myNum.toString()
+
+console.log(email)
